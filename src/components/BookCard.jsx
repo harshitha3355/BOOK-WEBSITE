7d234c577753d1f339a156240e26ab7b33646d94
harshitha3355 <harshitha3355@gmail.com>
2026-01-31 23:18:25 +0530

    changed header and book card, added search property, made book card display into grid

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -2,10 +2,12 @@ import './BookCard.css';
 
 function BookCard({ book }) {
   return (
-    <div className="book-card">
-      <img src={book.url} alt={book.title} className="book-img" />
-      <h2 className="book-title">{book.title}</h2>
-      <h2 className='book-author'>by {book.author}</h2>
+    <div className="book-disp">
+      <div className='book-card'>
+        <img src={book.url} alt={book.title} className="book-img" />
+        <h2 className="book-title">{book.title}</h2>
+        <h2 className='book-author'>by {book.author}</h2>
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,12 @@ import './Header.css';
 import { FaSearch } from 'react-icons/fa';
 import Line from './Line';
 import {useState} from 'react';
+import books from '../data/books'
+import BookCard from './BookCard'
 
 function Header(){
     const [searchVisible, setSearchVisible]=useState(false);
-    const [searchTerm, setSearchTerm]=useState(' ');
+    const [searchTerm, setSearchTerm]=useState("");
 
     const toggleSearch=()=>{setSearchVisible(!searchVisible)};
 
@@ -14,33 +16,44 @@ function Header(){
         setSearchTerm(event.target.value);
     };
 
+    const filteredBooks = books.filter(book => {
+        return book.title.toLowerCase().includes(searchTerm.trim().toLowerCase());
+    });
+
     return(
-        <header className='header'>
-            <div className='header-container'>
-                <h1 className="site-title">Page Trail</h1>
-                <nav className="nav"> 
-                    <a href="#home">Home</a>
-                    <a href="#books">Books</a>
-                    <a href="#blog">Blog</a>
-                    <a href="#fun">Fun</a>
-                    <a href="#about">About</a>
-                    <button onClick={toggleSearch} className="search-icon" aria-label="Toggle Search Input">
-                        <FaSearch id='search-icon' />
-                    </button>
-                </nav>
-            </div>
-            <Line />
-            {searchVisible && (
-                <input
-                type="text"
-                placeholder="Search..."
-                value={searchTerm}
-                onChange={handleInputChange}
-                className="search-input"
-                autoFocus 
-                />
-            )}
-        </header>
+        <>
+            <header className='header'>
+                <div className='header-container'>
+                    <h1 className="site-title">Page Trail</h1>
+                    <nav className="nav"> 
+                        <a href="#home">Home</a>
+                        <a href="#books">Books</a>
+                        <a href="#blog">Blog</a>
+                        <a href="#fun">Fun</a>
+                        <a href="#about">About</a>
+                        <button type="submit" onClick={toggleSearch} className="search-icon" aria-label="Toggle Search Input">
+                            <FaSearch id='search-icon' />
+                        </button>
+                    </nav>
+                </div>
+                <Line />
+                {searchVisible && (
+                    <>
+                        <input
+                            type="text"
+                            placeholder="Search for books, quizzes, challenges and blogs..."
+                            value={searchTerm}
+                            onChange={handleInputChange}
+                            className="search-input"
+                            autoFocus
+                        />
+                        {filteredBooks.map(book => (
+                            <BookCard key={book.id} book={book} />
+                        ))}
+                    </>
+                )}
+            </header>
+        </>
     );
 }
 export default Header;
