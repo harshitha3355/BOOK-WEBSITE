17521a77e21c06e97a54a11ba5ce55e61e39ff1d
harshitha3355 <harshitha3355@gmail.com>
2026-02-26 13:49:45 +0530

    search button fixed so that the display is in grid formats

--- src/components/SearchOverlay.jsx
+++ src/components/SearchOverlay.jsx
@@ -6,6 +6,9 @@ import FunCard from "./FunCard";
 import Subtitle from "./Subtitle";
 
 function SearchOverlay({ searchTerm,  onChange,  filteredBooks=[],  filteredBlogs=[],  filteredFun=[],  onClose}) {
+  
+  const hasResults = filteredBooks.length > 0 || filteredBlogs.length > 0 || filteredFun.length > 0;
+  
   return (
     <div className="search-overlay">
       <div className="search-panel">
@@ -21,31 +24,34 @@ function SearchOverlay({ searchTerm,  onChange,  filteredBooks=[],  filteredBlog
         <button className="close-btn" onClick={onClose}>✕</button>
 
         <div className="search-results">
+          {!hasResults && searchTerm && (
+            <div className="no-results">
+              No results found for "<span>{searchTerm}</span>"
+            </div>
+          )}
+          <Subtitle heading="Books"/>
           {filteredBooks.length > 0 && (
-            <>
-              <Subtitle heading="Books"/>
+            <div className="books-section">
               {filteredBooks.map(book => (
                 <BookCard key={book.id} book={book} />
               ))}
-            </>
+            </div>
           )}
-
+          <Subtitle heading="Blogs"/>
           {filteredBlogs.length > 0 && (
-            <>
-              <Subtitle heading="Blogs"/>
+            <div className="blogs-section">
               {filteredBlogs.map(blog => (
                 <BlogCard key={blog.id} blog={blog} />
               ))}
-            </>
+            </div>
           )}
-
+          <Subtitle heading="Fun"/>
           {filteredFun.length > 0 && (
-            <>
-              <Subtitle heading="Books"/>
+            <div className="fun-section">
               {filteredFun.map(f => (
                 <FunCard key={f.id} fun={f} />
               ))}
-            </>
+            </div>
           )}
         </div>
       </div>
