171463fa9481e118e7c7a5cb947b7ba7442db454
harshitha3355 <harshitha3355@gmail.com>
2026-03-15 17:17:21 +0530

    added new releses part to home pge, books data is extended and made few changes to book card

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -2,12 +2,10 @@ import './BookCard.css';
 
 function BookCard({ book }) {
   return (
-    <div className="book-disp">
-      <div className='book-card'>
-        <img src={book.url} alt={book.title} className="book-img" />
-        <h2 className="book-title">{book.title}</h2>
-        <h2 className='book-author'>by {book.author}</h2>
-      </div>
+    <div className="book-card">
+      <img src={book.url} alt={book.title} className="book-img" />
+      <h2 className="book-title">{book.title}</h2>
+      <h2 className="book-author">by {book.author}</h2>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Subtitle from "../components/Subtitle";
 import TrendingBlogs from "../components/TrendingBlogs";
+import books from "../data/books";
 import blogs from "../data/blogs";
+import Line from "../components/Line";
+import BookCard from "../components/BookCard";
 import "./Home.css";
 
 function Home() {
   return (
     <>
       <Header />
-
       <div className="home">
         <section className="trending-section">
           <Subtitle heading={"Trending Blogs"} />
           <TrendingBlogs blogs={blogs} />
+        </section>
+        <Line />
+        <section className="new-releases">
+          <Subtitle heading={"New Releases"} />
+          <div className="books-grid">
+            {books.slice(0, 24).map((book) => (
+              <BookCard key={book.id} book={book} />
+            ))}
+          </div>
+           <Link to="/books" className="link"><h2 className="view-more">View more</h2></Link>
         </section>
       </div>
 
