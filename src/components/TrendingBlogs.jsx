999a9028910e34024cf299d014941738b874730f
harshitha3355 <harshitha3355@gmail.com>
2026-03-15 14:18:58 +0530

    added trending blogs components and started with home page

---
+++ src/components/TrendingBlogs.jsx
@@ -0,0 +1,37 @@
+import "./TrendingBlogs.css";
+import Line from "./Line.jsx"
+
+function TrendingBlogs({ blogs }) {
+  const featuredBlog = blogs[0];
+  const sideBlogs = blogs.slice(1,4);
+  return (
+    <div className="trending-blogs-card">
+      {/* LEFT FEATURE BLOG */}
+      <div className="blog-card-left">
+        <img src={featuredBlog.image} alt={featuredBlog.title} className="blog-img" />
+        <div className="details">
+          <h2 className="blog-title">{featuredBlog.title}</h2>
+          <h2 className="blog-author">by {featuredBlog.author}</h2>
+          <p className="blog-content">
+            {featuredBlog.content}
+          </p>
+        </div>
+      </div>
+      <Line/>
+      {/* RIGHT BLOG LIST */}
+      <div className="blog-card-right">
+        {sideBlogs.map((blog) => (
+          <div className="side-blog" key={blog.id}>
+            <img src={blog.image} alt={blog.title} />
+            <div className="details-right">
+              <h2 className="blog-title">{blog.title}</h2>
+              <h2 className="blog-author">by {blog.author}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TrendingBlogs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Subtitle from "../components/Subtitle";
+import TrendingBlogs from "../components/TrendingBlogs";
+import blogs from "../data/blogs";
+import "./Home.css";
 
-function Home(){
-    return(
-        <>
-            <Header/>
-            <Footer/>
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <Header />
+
+      <div className="home">
+        <section className="trending-section">
+          <Subtitle heading={"Trending Blogs"} />
+          <TrendingBlogs blogs={blogs} />
+        </section>
+      </div>
+
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
